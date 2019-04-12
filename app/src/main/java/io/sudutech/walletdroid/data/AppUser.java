c168f3c1304f1be0a9c57bde5d7cbbf8787cf7d3
Mehdi <attarpour.mehdi@gmail.com>
2019-04-12 10:42:57 +0200

    Data class added

---
+++ app/src/main/java/io/sudutech/walletdroid/data/AppUser.java
@@ -0,0 +1,58 @@
+package io.sudutech.walletdroid.data;
+
+public class AppUser {
+    private int id;
+    private String name;
+    private String number;
+    private String email;
+    private int monthlyBudget;
+    private int monthlySave;
+
+
+
+    public AppUser(String name, String number, String email) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMonthlyBudget() {
+        return monthlyBudget;
+    }
+
+    public void setMonthlyBudget(int monthlyBudget) {
+        this.monthlyBudget = monthlyBudget;
+    }
+
+    public int getMonthlySave() {
+        return monthlySave;
+    }
+
+    public void setMonthlySave(int monthlySave) {
+        this.monthlySave = monthlySave;
+    }
+}
